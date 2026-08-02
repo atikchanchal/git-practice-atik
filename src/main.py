34f829b50f80d2ae25917ffe500ccd83dc7e7396
unknown <atikshimul45@gmail.com>
2026-08-02 22:09:42 +0600

    Add basic calculator function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from datetime import date
+from utils import add, subtract
 
-print("Name: Your Name")
+print("Name: Atik")
 print("Today's Date:", date.today())
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
