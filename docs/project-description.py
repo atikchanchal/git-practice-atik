46144279b1de814230c3653720a72fdbeed2929f
unknown <atikshimul45@gmail.com>
2026-08-02 22:42:17 +0600

    Update project documentation

---
+++ docs/project-description.py
@@ -0,0 +1,12 @@
+# Project Description
+
+This project is created for practicing Git and GitHub.
+
+It demonstrates:
+
+- Git initialization
+- Git commits
+- Branching
+- Merge
+- GitHub repository
+- Python calculator
